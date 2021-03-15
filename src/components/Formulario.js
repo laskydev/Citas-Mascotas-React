2c4e327daeb201a9329977730d63fefdc4dcfd47
laskydev <eduardofortuna99@gmail.com>
2021-03-15 10:54:28 -0600

    56 Evento para leer cuando el usuario envie el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -21,10 +21,20 @@ const Formulario = () => {
     //Extraer los valores
     const {mascota, propietario, fecha, hora, sintomas} = cita
 
+    //Cuando el usuario presiona agregar cita
+    const submitCita = (e) => {
+        e.preventDefault()
+        //Validar
+        //Asignar un ID
+        //Crear la cita
+        //Reiniciar el form
+    }
     return (
         <>
             <h1>Desde Formulario</h1>
-            <form>
+            <form
+                onSubmit={submitCita}
+            >
                 <label>Nombre Mascota</label>
                 <input
                     type="text"
